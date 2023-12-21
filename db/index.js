43556f70b1adf545744188fd1edf67d735aa9799
Mustafa Ahmed <mustafaahmed@Mustafas-Air-2.bbrouter>
2023-12-21 13:47:42 +0300

    second

--- db/index.js
+++ db/index.js
@@ -1,7 +1,11 @@
 const { Client } = require("pg");
 require("dotenv").config();
+const pgp = require("pg-promise")();
+
+const connectionString = process.env.client || "fallback-connection-string";
+const db = pgp(connectionString);
 const client = new Client({
-  connectionString: process.env.client,
+  connectionString,
 });
 client
   .connect()
